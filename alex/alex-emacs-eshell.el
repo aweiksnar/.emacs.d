aab68188d20638f00fa3313a42be5798313e99ad
Alex Weiksnar <aweiksnar@Alexs-MacBook-Pro.local>
2014-07-17 22:58:40 -0500

    move PATH getter to corresponding packagage file

--- alex/alex-emacs-eshell.el
+++ alex/alex-emacs-eshell.el
@@ -1,9 +1,5 @@
 ;;; alex-emacs-eshell.el --- eshell config
 
-;; get PATH
-(when (memq window-system '(mac ns))
-  (exec-path-from-shell-initialize))
-
 ;; fix eshell node repl weirdness
 (setenv "NODE_NO_READLINE" "1")
 
--- alex/alex-exec-path-from-shell.el
+++ alex/alex-exec-path-from-shell.el
@@ -1,4 +1,8 @@
 ;;; alex-exec-path-from-shell.el --- exec-path-from-shell
 (package-require 'exec-path-from-shell)
 
+;; get PATH
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
+
 (provide 'alex-exec-path-from-shell)
