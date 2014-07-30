52e798afd022b5c3ec81fcd70cf8f6c85b1b8a8e
Alex Weiksnar <alex@zooniverse.org>
2014-07-30 08:40:46 -0500

    windmove-default-keybindings

--- alex/alex-emacs-keys.el
+++ alex/alex-emacs-keys.el
@@ -3,6 +3,10 @@
 ;; toggle whitespace
 (global-set-key (kbd "C-x w") 'whitespace-mode)
 
+
+;; shift + arrow key to move between windows
+(windmove-default-keybindings)
+
 (put 'downcase-word 'disabled t)
 
 (provide 'alex-emacs-keys)
