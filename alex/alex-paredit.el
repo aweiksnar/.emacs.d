470f322a29416da3358df5f44ea08b945816c9d7
Alex Weiksnar <alex@zooniverse.org>
2014-09-09 12:41:06 -0500

    fix paredit hook

--- alex/alex-paredit.el
+++ alex/alex-paredit.el
@@ -2,6 +2,7 @@
 
 (package-require 'paredit)
 
+(autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
 (add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
 (add-hook 'clojure-mode-hook    'enable-paredit-mode)
 
