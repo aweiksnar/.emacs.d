09c8af6a04a7f2c9f2cecacb780df8e126dcaaeb
Alex Weiksnar <aweiksnar@gmail.com>
2014-09-30 21:55:33 -0500

    add om-mode

--- alex/alex-clojure.el
+++ alex/alex-clojure.el
@@ -3,6 +3,7 @@
 (package-require 'clojure-mode)
 (package-require 'clojure-test-mode)
 (package-require 'cider)
+(package-require 'om-mode)
 
 (add-to-list 'auto-mode-alist '("\\.cljs" . clojure-mode))
 
