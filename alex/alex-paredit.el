5ed87c8db9801cce20f8b0ada99bdbae81b9afd5
Alex Weiksnar <alex@zooniverse.org>
2014-11-10 13:47:22 -0600

    setup a list for paredit mode hooks

--- alex/alex-paredit.el
+++ alex/alex-paredit.el
@@ -3,7 +3,12 @@
 (package-require 'paredit)
 
 (autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
-(add-hook 'emacs-lisp-mode-hook #'enable-paredit-mode)
-(add-hook 'clojure-mode-hook    #'enable-paredit-mode)
+
+(setq paredit-hooks
+      '(emacs-lisp-mode-hook
+        clojure-mode-hook))
+
+(dolist (hook paredit-hooks)
+  (add-hook hook #'enable-paredit-mode))
 
 (provide 'alex-paredit)
