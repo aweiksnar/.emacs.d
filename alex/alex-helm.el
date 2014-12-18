bbda7b65bd71a328c911c96162afe98de3afcec1
Alex Weiksnar <aweiksnar@gmail.com>
2014-12-17 22:11:22 -0600

    bind helm-show-kill-ring to M-y

--- alex/alex-helm.el
+++ alex/alex-helm.el
@@ -4,6 +4,10 @@
 
 (global-set-key (kbd "C-c h") 'helm-find-files)
 
+;; M-y to helm-show-kill-ring
+;; http://sachachua.com/blog/2014/12/emacs-m-y-helm-show-kill-ring/
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+
 (helm-mode 1)
 
 (provide 'alex-helm)
