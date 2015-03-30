069e7e98f37bba50173345f2f8309d282beb2f9e
Alex Weiksnar <aweiksnar@gmail.com>
2015-03-29 19:01:09 -0500

    remove yasnippet-bundle, clojure-test-mode

--- init.el
+++ init.el
@@ -2,7 +2,7 @@
 
 (require 'org-install)
 (require 'ob-tangle)
-;; (setq debug-on-error t)
+(setq debug-on-error t)
 (org-babel-load-file
  (expand-file-name "alex.org"
                    user-emacs-directory))
