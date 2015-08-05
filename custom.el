e3ec17e69735c67fcec4e04ccc64682681f37e37
Alex Weiksnar <aweiksnar@gmail.com>
2015-08-04 19:50:19 -0500

    web mode intentation update, .dir-locals.el custom

--- custom.el
+++ custom.el
@@ -15,7 +15,13 @@
  '(grep-find-ignored-files
    (quote
     (".#*" "*.o" "*~" "*.bin" "*.lbin" "*.so" "*.a" "*.ln" "*.blg" "*.bbl" "*.elc" "*.lof" "*.glo" "*.idx" "*.lot" "*.fmt" "*.tfm" "*.class" "*.fas" "*.lib" "*.mem" "*.x86f" "*.sparcf" "*.dfsl" "*.pfsl" "*.d64fsl" "*.p64fsl" "*.lx64fsl" "*.lx32fsl" "*.dx64fsl" "*.dx32fsl" "*.fx64fsl" "*.fx32fsl" "*.sx64fsl" "*.sx32fsl" "*.wx64fsl" "*.wx32fsl" "*.fasl" "*.ufsl" "*.fsl" "*.dxl" "*.lo" "*.la" "*.gmo" "*.mo" "*.toc" "*.aux" "*.cp" "*.fn" "*.ky" "*.pg" "*.tp" "*.vr" "*.cps" "*.fns" "*.kys" "*.pgs" "*.tps" "*.vrs" "*.pyc" "*.pyo" "bundle.js")))
- '(org-agenda-files (quote ("~/.emacs.d/alex.org"))))
+ '(org-agenda-files (quote ("~/.emacs.d/alex.org")))
+ '(safe-local-variable-values
+   (quote
+    ((eval setq web-mode-content-types-alist
+           (\`
+            (("jsx" \,
+              (concat default-directory ".*\\.js")))))))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
