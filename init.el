fbf8a6c18d551c31335cfa14cd280fa7d41d8dd2
Alex Weiksnar <aweiksnar@gmail.com>
2016-01-14 16:13:50 -0600

    large conversion to use-package format

--- init.el
+++ init.el
@@ -1,10 +1,11 @@
 ;;; init.el --- alex's init file
 
-;; (package-initialize) in alex.org, commented here for emacs
+(require 'package)
+(setq package-enable-at-startup nil)
+(package-initialize)
 
-(require 'org-install)
-(require 'ob-tangle)
 ;; (setq debug-on-error t)
+
 (org-babel-load-file
  (expand-file-name "alex.org"
                    user-emacs-directory))
